ffb7b0c74f5c5e982175af0aeff0c5788cd7a729
JarvisMoritz <105207945+JarvisMoritz@users.noreply.github.com>
2022-07-08 17:37:43 +0200

    Added Froude number

--- compute_CFL_number.m
+++ compute_CFL_number.m
@@ -1,8 +1,9 @@
-function [CFL_max] = compute_CFL_number(constants, grid, dt, h, hu, hv)
+function [CFL_max, Fr_max] = compute_CFL_number(constants, grid, dt, h, hu, hv)
 %COMPUTE_CFL_NUMBER Computes the CFL number. This is probably not perfect.
 
 
 CFL_max = 0;
+Fr_max =0;
 
 for j = grid.NGHOST+1:length(grid.y)-grid.NGHOST
     % The i-loop is inside the j-loop because of the column major memory
@@ -15,12 +16,12 @@ for j = grid.NGHOST+1:length(grid.y)-grid.NGHOST
         % TODO: Compute CFL
         u_res = max(abs(u+c),abs(u-c));
         CFL = (u_res*dt/grid.dx) + (v*dt/grid.dy);
+        Fr = u/sqrt(constants.g*h(i,j));
 
         % use worst case
         CFL_max = max( CFL, CFL_max );
-
+        Fr_max = max(Fr,Fr_max);
     end
 end
 
 end
-
