08e723ad7db2520737f7d3b0a2330c845573418d
JarvisMoritz <105207945+JarvisMoritz@users.noreply.github.com>
2022-07-08 17:38:03 +0200

    Update set_boundary_conditions.m

--- set_boundary_conditions.m
+++ set_boundary_conditions.m
@@ -1,4 +1,4 @@
-function [ bconds ] = set_boundary_conditions()
+function [ bconds ] = set_boundary_conditions(constants)
 %SET_BOUNDARY_CONDITIONS Set boundary conditions
 
 bconds = struct();
@@ -9,7 +9,7 @@ bconds.bnorth = {'PER'};
 bconds.bsouth = {'PER'};
 
 % TODO: Insert values for boundary condition
-bconds.hwest = 1
-bconds.huwest = 30*0.001^(1/2)
+bconds.hwest = constants.h
+bconds.huwest = constants.kst*constants.I^(1/2)*constants.h^(5/3)
 end
 
